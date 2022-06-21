dcf01a10269a02b475f80a2236c527be9cdfbcd9
puneet56 <puneetdhiman56@gmail.com>
2022-06-21 22:37:29 +0530

    Loaded model and can recognize image

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 import * as tf from "@tensorflow/tfjs-node";
+import automl from "@tensorflow/tfjs-automl";
 import express from "express";
-import modelData from "./tensorflow/model.json" assert { type: "json" };
+import axios from "axios";
+// import modelData from "./tensorflow/model.json" assert { type: "json" };
 import fs from "fs";
+
+const handler = tf.io.fileSystem("./tensorflow/model.json");
 
 const port = 4000;
 
 async function run() {
-	const image = fs.readFile("./pikachu.png", async (err, data) => {
-		if (err) {
-			console.log(err);
-		} else {
-			const model = await tf.automl.loadImageClassification(modelData);
-			//get image to test
-			const predictions = await model.classify(image);
-			//sort prediction by highest prob, take the first one
-			predictions.sort(sortByProperty("prob"));
-			//Add prediction result inside output element
-
-			return predictions;
-		}
-	});
+	const data = fs.readFileSync("./pokemon.jpg");
+	const image = tf.node.decodeImage(data, 3);
+	const graphModel = await tf.loadGraphModel(handler);
+	let dict = fs.readFileSync("./tensorflow/dict.txt", "utf8");
+	dict = dict.split("\r\n");
+	const model = new automl.ImageClassificationModel(graphModel, dict);
+	const predictions = await model.classify(image);
+	predictions.sort((a, b) => b.prob - a.prob);
+	return predictions;
 }
 
-async function runServer() {
-	const app = express();
+const app = express();
 
-	app.use(express.json());
+app.use(express.json());
 
-	app.get("*", async (req, res) => {
-		const data = await run();
-		res.json(data);
-	});
+app.get("*", async (req, res) => {
+	const data = await run();
+	res.send(data);
+});
 
-	app.listen(port, () => {
-		console.log(`Listening on port ${port}`);
-	});
-}
-
-runServer();
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
