3f489f63af26391f23d641d0c84d3cea6b5c199b
Puneet <puneetdhiman56@gmail.com>
2022-06-22 12:08:26 +0530

    Api working added multer

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 import * as tf from "@tensorflow/tfjs-node";
 import automl from "@tensorflow/tfjs-automl";
 import express from "express";
-import axios from "axios";
-// import modelData from "./tensorflow/model.json" assert { type: "json" };
 import fs from "fs";
+import multer from "multer";
+import morgan from "morgan";
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./uploads");
+	},
+	filename: function (req, file, cb) {
+		cb(null, `${Date.now()}-${file.originalname}`);
+	},
+});
+
+const upload = multer({ storage });
 
 const handler = tf.io.fileSystem("./tensorflow/model.json");
 
 const port = 4000;
 
-async function run() {
-	const data = fs.readFileSync("./pokemon.jpg");
+async function run(data) {
 	const image = tf.node.decodeImage(data, 3);
 	const graphModel = await tf.loadGraphModel(handler);
 	let dict = fs.readFileSync("./tensorflow/dict.txt", "utf8");
-	dict = dict.split("\r\n");
-	const model = new automl.ImageClassificationModel(graphModel, dict);
+	let dictArray = dict.split("\n");
+	const model = new automl.ImageClassificationModel(graphModel, dictArray);
 	const predictions = await model.classify(image);
 	predictions.sort((a, b) => b.prob - a.prob);
 	return predictions;
@@ -23,11 +33,15 @@ async function run() {
 
 const app = express();
 
+app.use(morgan("dev"));
+
 app.use(express.json());
 
-app.get("*", async (req, res) => {
-	const data = await run();
-	res.send(data);
+app.post("/", upload.single("image"), async (req, res) => {
+	const image = fs.readFileSync(req.file.path);
+	const predictions = await run(image);
+	fs.unlink(req.file.path, () => {});
+	res.json(predictions);
 });
 
 app.listen(port, () => {
